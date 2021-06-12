882b336831feee2b676d228a906f6f245a72716f
Julian Kominovic <juliankominovic@gmail.com>
2021-06-11 23:01:17 -0300

    Mejorado el trigger del trailer y la navbar ahora se torna color negro como en el sitio de netflix ORIGINAL

--- main.js
+++ main.js
@@ -6,43 +6,73 @@ const trailerDescription = document.querySelector(".movie__trailer-description")
 const trailerCoverImage = document.querySelector('.movie__trailer-cover');
 const movieCatalogue = document.querySelector(".main__body");
 
-const adjustTrailerInfo = ()=>{
+const toggleTrailerInfo = ()=>{
     console.log("AJUSTANDO")
     trailerName.classList.toggle("trailer__playing-title");
     trailerDescription.classList.toggle("trailer__playing-description");
     trailerCoverImage.classList.toggle("trailer__playing-cover");
 }
 
+const setOfftrailerInfo = ()=>{
+    trailerName.classList.remove("trailer__playing-title");
+    trailerDescription.classList.remove("trailer__playing-description");
+    trailerCoverImage.classList.remove("trailer__playing-cover");
+}
+
+const setOntrailerInfo = ()=>{
+    trailerName.classList.add("trailer__playing-title");
+    trailerDescription.classList.add("trailer__playing-description");
+    trailerCoverImage.classList.add("trailer__playing-cover");
+}
+
 const initTrailerPlayer = (e)=>{
-    setTimeout(()=>{trailerVideo.play()
-        adjustTrailerInfo()}, 4000);
+        trailerVideo.play()
 }
 
 const stopTrailerPlayer = ()=>{
     console.log("parando a moto");
     trailerVideo.pause();
-    adjustTrailerInfo();
-    trailerVideo.removeEventListener('loadeddata', (e)=>{
-        setTimeout(()=>initTrailerPlayer(e), 4000);
-    });
+ 
 
 }
 
-const adjustNavBar = ()=>{
+const startTrailer = ()=>{
+    if(trailerVideo.paused){
+        initTrailerPlayer();
+        setOntrailerInfo(); 
+    }
+}
 
-    if(window.scrollY!=0){
-        navBar.setAttribute("style", "background-color: rgb(20,20,20)");
+const stopTrailer = ()=>{
+    if(!trailerVideo.paused){
+        setOfftrailerInfo();
+        stopTrailerPlayer();
+    }
+}
+
+
+const whenOnTop = ()=>{
+    console.log(window.pageYOffset)
+    if(window.pageYOffset > 30){
+        stopTrailer();
     } else{
-        navBar.setAttribute("style", "background:linear-gradient(0deg, rgba(20, 20, 20, 0), rgba(20, 20, 20, 255))");
+        startTrailer(); 
     } 
+}
 
+const toggleNavBar = ()=>{
+    window.pageYOffset > 0 ?  navBar.setAttribute("style", "background-color: rgb(20,20,20)") : navBar.setAttribute("style", "background:linear-gradient(0deg, rgba(20, 20, 20, 0), rgba(20, 20, 20, 255))");
 }
 
 // EVENTS
-window.addEventListener('scroll', ()=>adjustNavBar());
-trailerVideo.addEventListener('loadeddata',()=>initTrailerPlayer());
-trailerVideo.addEventListener('mouseover',()=>initTrailerPlayer());
-movieCatalogue.addEventListener('mouseover',()=>stopTrailerPlayer());
+window.addEventListener('scroll', ()=>{
+    whenOnTop();
+    toggleNavBar();
+});
+trailerVideo.addEventListener('loadeddata', ()=>setTimeout(whenOnTop,4000));
+
+
+
 
 
 function saludo(){
