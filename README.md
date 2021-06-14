f02f6ff444e562dd6231208619948ad5810d181c
Julian Kominovic <juliankominovic@gmail.com>
2021-06-14 02:27:34 -0300

    creado parcialmente el sistema dinamico de carga de videos con un REST API fake del sitio mockAPI, donde hago un metodo GET para solicitar la informacion de las peliculas

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 
-This is a Netflix website clone, made in Vanilla CSS and Javascript for educational purpose, the real website's domain is www.netflix.com.
+This is a Netflix website clone, made in Vanilla CSS and JavaScript for educational purpose, the real website's domain is www.netflix.com.
 
 Technologies applied:
 
@@ -10,3 +10,9 @@ Technologies applied:
 - JavaScript (ECMA Script 11, vanilla).
 - CSS (vanilla, BEM methodology).
 - JSON (to simulate server requests).
+
+
+
+Fake requests made from MockAPI with AXIOS library. These request return movie's information and is used to dynamically create new tabs and content for each movie genre.
+
+JSON fake data were created with Mockaroo and images from Lorem Picsum.
--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const trailerDescription = document.querySelector(".movie__trailer-description")
 const trailerCoverImage = document.querySelector('.movie__trailer-cover');
 const movieCatalogue = document.querySelector(".main__body");
 
+//FUNCTIONS
+
 const toggleTrailerInfo = ()=>{
-    console.log("AJUSTANDO")
     trailerName.classList.toggle("trailer__playing-title");
     trailerDescription.classList.toggle("trailer__playing-description");
     trailerCoverImage.classList.toggle("trailer__playing-cover");
@@ -30,7 +31,6 @@ const initTrailerPlayer = (e)=>{
 }
 
 const stopTrailerPlayer = ()=>{
-    console.log("parando a moto");
     trailerVideo.pause();
  
 
@@ -52,7 +52,6 @@ const stopTrailer = ()=>{
 
 
 const whenOnTop = ()=>{
-    console.log(window.pageYOffset)
     if(window.pageYOffset > 30){
         stopTrailer();
     } else{
@@ -63,6 +62,95 @@ const whenOnTop = ()=>{
 const toggleNavBar = ()=>{
     window.pageYOffset > 0 ?  navBar.setAttribute("style", "background-color: rgb(20,20,20)") : navBar.setAttribute("style", "background:linear-gradient(0deg, rgba(20, 20, 20, 0), rgba(20, 20, 20, 255))");
 }
+
+
+//-----------------------------FAKE REQUEST MOCK API (for fake movies)-------------------------
+// -------- Lets build the movies section ----------
+let moviesRequested = [];
+let moviesHtmlContainers = [];
+let fragment = new DocumentFragment();
+
+
+
+
+const buildMovieCatalogue = ()=>{
+    requestMovies(moviesRequested).then(
+        (movies)=>{
+            movies.map((e)=>{
+                BuildHtmlCatalogue(e);
+            })
+        }
+        )
+    }
+
+//some error with json request doesnt let me compare properly the html fragments id and the movie genres, so i buit this function
+
+const isInside = (id, movieGenres)=>{
+    let separador = ' ';
+    let matches = 0;
+    let moviesGenSplit = movieGenres.split(separador);
+    moviesGenSplit.map((e)=>{
+        e == id ? matches++ : null;
+    });
+    return matches;
+}
+
+const BuildHtmlCatalogue = (element)=>{
+    let newELement = document.createElement('li');
+    newELement.className = "movie__container-item";
+    moviesHtmlContainers.forEach(frag => {
+        if(isInside(frag.id, element.movie_genre) >= 1){
+            console.log(frag);
+            console.log(element.movie_title);
+            newELement.innerHTML = `
+                        <a href="" class="movie__container-item-clickeable">
+                            <div class="movie__container-item-cover">
+                                <img class="movie__container-item-img" src="${element.movie_cover}" alt="${element.movie_title}">
+                            </div>
+                        </a>
+    `;
+            frag.firstElementChild.nextElementSibling.appendChild(newELement);
+        }
+    });
+}
+    
+const requestMovies = (moviesRequested)=>{
+    return new Promise((resolve, reject)=>{
+        let request = axios.get('https://60c6a86f19aa1e001769f896.mockapi.io/netflix-clone/movies')
+        .then((res)=>{
+            moviesRequested = res.data;
+            resolve(moviesRequested)
+        });
+    })
+}
+
+buildMovieCatalogue();
+
+
+//-----------------------------DINAMIC MOVIE CATALOGUE-------------------------
+
+
+
+const genres = ['Comedy', 'Horror', 'Drama', 'Romance', 'Sci-Fi', 'Thriller'];
+
+const createMovieContainers = (genres)=>{
+    fragment.innerHTML = "";
+    genres.map((elem)=>{
+        fragment.innerHTML += `<div class="movie__container" id="${elem}">
+        <a class="movie__container-category-name" href="#">${elem}</a>
+        <ul class="movie__container-list">
+        </ul>
+        </div>`
+    })
+    movieCatalogue.innerHTML += fragment.innerHTML
+    moviesHtmlContainers = document.querySelectorAll('.movie__container')
+    console.log(fragment.innerHTML) //---- bien
+    console.log(moviesHtmlContainers)//----bien
+}
+
+
+
+createMovieContainers(genres);
 
 // EVENTS
 window.addEventListener('scroll', ()=>{
